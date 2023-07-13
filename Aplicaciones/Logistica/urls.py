1219c422142cba15e557b508fa8ef34d6acb358f
santiceaglio <santiceagliohf@gmail.com>
2023-07-12 22:20:37 -0300

    funciona el searchbar

--- Aplicaciones/Logistica/urls.py
+++ Aplicaciones/Logistica/urls.py
@@ -1,6 +1,13 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home)
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home')
 ]
--- Aplicaciones/Logistica/views.py
+++ Aplicaciones/Logistica/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Producto
 
-# Create your views here.
+# # Create your views here.
+
+# def home(request):
+#     productosListados = Producto.objects.all()
+#     return render(request, "gestionProductos.html", {"productos": productosListados})
 
 def home(request):
-    productosListados = Producto.objects.all()
+    search_query = request.GET.get('search_query', '')  # Obtener el término de búsqueda de request.GET
+
+    if search_query:
+        productosListados = Producto.objects.filter(nombre__icontains=search_query)
+    else:
+        productosListados = Producto.objects.all()
+    
     return render(request, "gestionProductos.html", {"productos": productosListados})
